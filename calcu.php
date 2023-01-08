c5c0dc5ad64afe413dde3382218bfd7207c7d77d
Eduardo Esparza <lalopenaesparza@gmail.com>
2023-01-08 12:40:33 -0600

    :v

---
+++ calcu.php
@@ -0,0 +1,41 @@
+<?php
+
+$operacion = $_GET['operacion'];
+$resultado = $_GET['resultado'];
+
+//echo $operacion;
+//echo $resultado;
+//echo $operacion === "" ? "Vacio": $var;
+//echo $resultado === "" ? "Vacio": $var;
+
+$user = "root";
+$pass = "";
+$host = "localhost";
+$connection = mysqli_connect($host, $user, $pass);
+
+if(!$connection) {
+    echo "No se ha podido conectar con el servidor";
+}else{
+    echo "Hemos conectado al servidor" ;
+}
+
+$datab = "ejemplo";
+$db = mysqli_select_db($connection,$datab);
+
+/*if (!$db){
+    echo "No se ha podido encontrar la Tabla";
+}else{
+    echo "Tabla seleccionada" ;
+}*/
+
+echo $operacion;
+
+$insercion = "INSERT INTO calculadora (operacion,resultado) VALUES ('$operacion','$resultado')";
+$result = mysqli_query($connection,$insercion);
+
+if(!$result) {
+    echo "No se ha podido realizar la insercion";
+}
+mysqli_close($connection);
+echo "Fuera" ;
+?>
---
+++ calculator.js
@@ -0,0 +1,44 @@
+let screen = document.querySelector('#screen-result')
+let operacion
+let resultado
+var operacion_
+
+function getData(ref) {
+    let value = ref.value;
+    screen.value += value;
+}
+
+function clean() {
+    screen.value = '';
+}
+
+function enviarDatos() {
+
+    const url = "calcu.php?operacion="
+    let xhr = new XMLHttpRequest()
+
+    xhr.open("GET", url+operacion_+"&resultado="+resultado, true);
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    
+    xhr.send();
+}
+
+function calculate() {
+    try {
+        operacion = screen.value;
+        operacion_ = operacion.toString()
+        screen.value = eval(screen.value);
+        //alert(operacion);
+        resultado = screen.value
+        //alert(resultado)
+        enviarDatos();
+    } catch (error) {
+        screen.value = 'Error'
+    }
+}
+
